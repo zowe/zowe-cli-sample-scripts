a2b9214388dbf5cab1a7c9bc07e664dabb3da424
Guilherme Cartier <gcartier@br.ibm.com>
2019-08-12 18:56:16 +0000

    Included License header EPL-2.0 to source code
    Signed-off-by: Guilherme Cartier <gcartier@br.ibm.com>

--- Twilio/Twilio-CA-ESP-Sample/env_scripts/src/watch.ts
+++ Twilio/Twilio-CA-ESP-Sample/env_scripts/src/watch.ts
@@ -1,6 +1,15 @@
 #! /bin/env node
-// Script used to watch the source folder and upload the changes to z/OS automatically
+/*
+* This program and the accompanying materials are made available under the terms of the *
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at  *
+* https://www.eclipse.org/legal/epl-v20.html                                            *
+*                                                                                       *
+* SPDX-License-Identifier: EPL-2.0                                                      *
+*                                                                                       *
+* Copyright Contributors to the Zowe Project.                                           *
+*/
 
+// Script used to watch the source folder and upload the changes to z/OS automatically
 import {executeCommand} from "./executeCommand"
 import * as config from "config";
 const nodemon = require("nodemon");
