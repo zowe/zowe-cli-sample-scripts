6a8eda0bf8a1be6d8138a9915eae582ccf564f7b
zFernand0 <fernando.rijocedeno@broadcom.com>
2019-08-12 17:12:10 -0400

    Add the same license header for python and shell
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

--- shell/push.sh
+++ shell/push.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+#
+# This program and the accompanying materials are made available under the terms of the #
+# Eclipse Public License v2.0 which accompanies this distribution, and is available at  #
+# https://www.eclipse.org/legal/epl-v20.html                                            #
+#                                                                                       #
+# SPDX-License-Identifier: EPL-2.0                                                      #
+#                                                                                       #
+# Copyright Contributors to the Zowe Project.                                           #
+#
+
 
 #upload code
 bright endevor update element COBLSAMP --env dev --sys ccs --sub demo --typ cobpgm --ff samp.cbl --i WEBSMFNE --sn 1 --ccid share --com "SHARE demo"
