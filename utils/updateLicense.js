a5fa2224d7d9bfe9d36f7c06b619540b3c306828
Gene Johnston <eugene.johnston@broadcom.com>
2021-08-27 16:13:03 -0400

    ESLint updates for top-level package.json
    Signed-off-by: Gene Johnston <Eugene.Johnston@broadcom.com>

--- utils/updateLicense.js
+++ utils/updateLicense.js
@@ -14,53 +14,53 @@ const path = require("path");
 
 // process all typescript files
 require("glob")("{*,!(node_modules)/}**/{*.js,*.ts,Jenkinsfile,*.py,*.sh}", (globErr, filePaths) => {
-        if (globErr) {
-            throw globErr;
-        }
-        // turn the license file into a multi line comment
-        const desiredLineLength = 86;
-        let alreadyContainedCopyright = 0;
-        const jsHeader = "/*\n" + fs.readFileSync("LICENSE_HEADER").toString()
-                .split(/\r?\n/g).map((line) => {
-                    const lenAdjust = desiredLineLength - line.length;
-                    const pad = Array((lenAdjust < 0) ? 0 : lenAdjust).join(" ");
-                    return " * " + line + pad + " *";
-                })
-                .join(os.EOL) + os.EOL + " */" + os.EOL + os.EOL;
-        const shHeader = "#\n" + fs.readFileSync("LICENSE_HEADER").toString()
-            .split(/\r?\n/g).map((line) => {
-                const lenAdjust = desiredLineLength - line.length;
-                const pad = Array((lenAdjust < 0) ? 0 : lenAdjust).join(" ");
-                return "# " + line + pad + " #";
-            })
-            .join(os.EOL) + os.EOL + "#" + os.EOL + os.EOL;
-        
-        for (const filePath of filePaths) {
-            let useShHeader = false;
-            if (path.extname(filePath) === ".sh" || path.extname(filePath) === ".py"){
-                useShHeader = true;
-            }
-            const file = fs.readFileSync(filePath);
-            let result = file.toString();
-            const resultLines = result.split(/\r?\n/g);
-            if (resultLines.join().indexOf((useShHeader ? shHeader : jsHeader).split(/\r?\n/g).join()) >= 0) {
-                alreadyContainedCopyright++;
-                continue; // already has copyright
-            }
-            const shebangPattern = require("shebang-regex");
-            let usedShebang = "";
-            result = result.replace(shebangPattern, function (fullMatch) {
-                usedShebang = fullMatch + "\n"; // save the shebang that was used, if any
-                return "";
-            });
-            // remove any existing copyright
-            // Be very, very careful messing with this regex. Regex is wonderful.
-            result = result.replace(/\/\*[\s\S]*?(License|SPDX)[\s\S]*?\*\/[\s\n]*/i, "");
-            result = (useShHeader ? shHeader : jsHeader) + result; // add the new header
-            result = usedShebang + result; // add the shebang back
-            fs.writeFileSync(filePath, result);
-        }
-        console.log("Ensured that %d files had copyright information" +
-            " (%d already did).", filePaths.length, alreadyContainedCopyright);
+    if (globErr) {
+        throw globErr;
     }
+    // turn the license file into a multi line comment
+    const desiredLineLength = 86;
+    let alreadyContainedCopyright = 0;
+    const jsHeader = "/*\n" + fs.readFileSync("LICENSE_HEADER").toString()
+        .split(/\r?\n/g).map((line) => {
+            const lenAdjust = desiredLineLength - line.length;
+            const pad = Array((lenAdjust < 0) ? 0 : lenAdjust).join(" ");
+            return " * " + line + pad + " *";
+        })
+        .join(os.EOL) + os.EOL + " */" + os.EOL + os.EOL;
+    const shHeader = "#\n" + fs.readFileSync("LICENSE_HEADER").toString()
+        .split(/\r?\n/g).map((line) => {
+            const lenAdjust = desiredLineLength - line.length;
+            const pad = Array((lenAdjust < 0) ? 0 : lenAdjust).join(" ");
+            return "# " + line + pad + " #";
+        })
+        .join(os.EOL) + os.EOL + "#" + os.EOL + os.EOL;
+
+    for (const filePath of filePaths) {
+        let useShHeader = false;
+        if (path.extname(filePath) === ".sh" || path.extname(filePath) === ".py") {
+            useShHeader = true;
+        }
+        const file = fs.readFileSync(filePath);
+        let result = file.toString();
+        const resultLines = result.split(/\r?\n/g);
+        if (resultLines.join().indexOf((useShHeader ? shHeader : jsHeader).split(/\r?\n/g).join()) >= 0) {
+            alreadyContainedCopyright++;
+            continue; // already has copyright
+        }
+        const shebangPattern = require("shebang-regex");
+        let usedShebang = "";
+        result = result.replace(shebangPattern, function (fullMatch) {
+            usedShebang = fullMatch + "\n"; // save the shebang that was used, if any
+            return "";
+        });
+        // remove any existing copyright
+        // Be very, very careful messing with this regex. Regex is wonderful.
+        result = result.replace(/\/\*[\s\S]*?(License|SPDX)[\s\S]*?\*\/[\s\n]*/i, "");
+        result = (useShHeader ? shHeader : jsHeader) + result; // add the new header
+        result = usedShebang + result; // add the shebang back
+        fs.writeFileSync(filePath, result);
+    }
+    console.log("Ensured that %d files had copyright information" +
+        " (%d already did).", filePaths.length, alreadyContainedCopyright);
+}
 );
