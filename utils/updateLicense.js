f616cb1f71e96a623ff776a10b5457396410409c
Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>
2025-11-15 14:29:01 -0500

    fix license headers
    Signed-off-by: Fernando Rijo Cedeno <37381190+zFernand0@users.noreply.github.com>

--- utils/updateLicense.js
+++ utils/updateLicense.js
@@ -2,22 +2,21 @@
  * This program and the accompanying materials are made available and may be used, at your option, under either:
  * - Eclipse Public License v2.0, available at https://www.eclipse.org/legal/epl-v20.html, OR
  * - Apache License, version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
- *
+ * 
  * Copyright Contributors to the Zowe Project.
- *
+ * 
  */
 
 const os = require("os");
 const fs = require("fs");
 const path = require("path");
 
-// process all typescript files
-require("glob")("{*,!(node_modules)/}**/{*.js,*.ts,Jenkinsfile,*.py,*.sh}", (globErr, filePaths) => {
-    if (globErr) {
-        throw globErr;
-    }
+(async () => {
+    // process all typescript files
+    const filePaths = await require("glob").glob("**/{*.js,*.ts,Jenkinsfile,*.py,*.sh}", { ignore: "**/node_modules/**" });
+
     // turn the license file into a multi line comment
     let alreadyContainedCopyright = 0;
     const jsHeader = "/*\n" + fs.readFileSync("LICENSE_HEADER").toString()
@@ -51,12 +50,19 @@ require("glob")("{*,!(node_modules)/}**/{*.js,*.ts,Jenkinsfile,*.py,*.sh}", (glo
         });
         // remove any existing copyright
         // Be very, very careful messing with this regex. Regex is wonderful.
-        result = result.replace(/\/\*[\s\S]*?(License|SPDX)[\s\S]*?\*\/[\s\n]*/i, "");
+        if (useShHeader) {
+            // Remove shell/Python style comments containing license info
+            // Match all consecutive comment lines that are part of a license block
+            result = result.replace(/(^#[^\n]*\n)+(^#[^\n]*(?:License|SPDX|Copyright|Eclipse|Apache)[^\n]*\n)(^#[^\n]*\n)*/gim, "");
+        } else {
+            result = result.replace(/\/\*[\s\S]*?(License|SPDX)[\s\S]*?\*\/[\s\n]*/i, "");
+        }
+        // Trim leading whitespace/newlines from the content
+        result = result.replace(/^\s+/, "");
         result = (useShHeader ? shHeader : jsHeader) + result; // add the new header
         result = usedShebang + result; // add the shebang back
         fs.writeFileSync(filePath, result);
     }
     console.log("Ensured that %d files had copyright information" +
         " (%d already did).", filePaths.length, alreadyContainedCopyright);
-}
-);
+})();
